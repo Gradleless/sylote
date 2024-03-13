44382dbec35a0318840fdc1467f00ddb8d111daa
Lyciaa <57102194+Mavenless@users.noreply.github.com>
2024-03-13 19:29:18 +0100

    Ajout de la fonctionnalité de saisie utilisateur et de sauvegarde des données

---
+++ lib/cli/inputs.go
@@ -0,0 +1,142 @@
+package cli
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+)
+
+type Data struct {
+	Search         []string `json:"search"`
+	ID             string   `json:"id"`
+	Update         bool     `json:"update"`
+	Time           string   `json:"time"`
+	DiscordWebhook string   `json:"discordWebhook"`
+}
+
+func GetUserInput() (string, string, bool, string, string) {
+	var searchStr string
+	fmt.Print("Entrez les termes de recherche (séparés par des virgules) : ")
+	fmt.Scanln(&searchStr)
+
+	var id string
+	fmt.Print("Entrez l'ID : ")
+	fmt.Scanln(&id)
+
+	var update bool
+	fmt.Print("Mettre à jour ? (true/false) : ")
+	_, err := fmt.Scanln(&update)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return "", "", false, "", ""
+	}
+
+	if reflect.TypeOf(update).Kind() != reflect.Bool {
+		fmt.Println("Erreur : update doit être un booléen (true/false)")
+		return "", "", false, "", ""
+	}
+
+	var timeStr string
+
+	if !update {
+		fmt.Print("Entrez l'heure (AAAA-MM-JJ) : ")
+		_, err := fmt.Scanln(&timeStr)
+		if err != nil {
+			fmt.Println("Erreur :", err)
+			return "", "", false, "", ""
+		}
+	}
+
+	var discordWebhook string
+	fmt.Print("Entrez l'URL du webhook Discord : ")
+	fmt.Scanln(&discordWebhook)
+
+	return searchStr, id, update, timeStr, discordWebhook
+}
+
+func CreateData(searchStr string, id string, update bool, timeStr string, discordWebhook string) struct {
+	Search         []string `json:"search"`
+	ID             string   `json:"id"`
+	Update         bool     `json:"update"`
+	Time           string   `json:"time"`
+	DiscordWebhook string   `json:"discordWebhook"`
+} {
+	search := strings.Split(searchStr, ",")
+	data := struct {
+		Search         []string `json:"search"`
+		ID             string   `json:"id"`
+		Update         bool     `json:"update"`
+		Time           string   `json:"time"`
+		DiscordWebhook string   `json:"discordWebhook"`
+	}{
+		Search:         search,
+		ID:             id,
+		Update:         update,
+		Time:           timeStr,
+		DiscordWebhook: discordWebhook,
+	}
+	return data
+}
+
+func SaveDataToFile(data struct {
+	Search         []string `json:"search"`
+	ID             string   `json:"id"`
+	Update         bool     `json:"update"`
+	Time           string   `json:"time"`
+	DiscordWebhook string   `json:"discordWebhook"`
+}) {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return
+	}
+
+	err = os.Mkdir("data", 0755)
+	if err != nil {
+		fmt.Println("Error creating folder:", err)
+		return
+	}
+
+	file, err := os.Create("data/data.json")
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Write(jsonData)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return
+	}
+}
+
+func ReadDataFromFile() Data {
+	file, err := os.Open("data/data.json")
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return Data{}
+	}
+	defer file.Close()
+
+	fileData := make([]byte, 0)
+	buffer := make([]byte, 1024)
+	for {
+		n, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+		fileData = append(fileData, buffer[:n]...)
+	}
+
+	data := Data{}
+	err = json.Unmarshal(fileData, &data)
+	if err != nil {
+		fmt.Println("Erreur :", err)
+		return Data{}
+	}
+
+	return data
+}
