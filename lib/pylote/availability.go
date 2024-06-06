e85a27894085fb7bb171ae87848db40b4a3eb669
Lyciaa <57102194+Gradleless@users.noreply.github.com>
2024-06-06 18:32:27 +0200

    updated availability, pylote api needs to write a custom variable to update it

--- lib/pylote/availability.go
+++ lib/pylote/availability.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"time"
 )
 
 func SetAvailability(id string, available bool, date string) {
@@ -27,18 +28,12 @@ func SetAvailability(id string, available bool, date string) {
 		return
 	}
 
-	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36")
+	req.Header.Add("User-Agent", "Sylote")
 	req.Header.Add("Accept", "application/json, text/plain, */*")
+	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Accept-Language", "en-US,en;q=0.5")
 	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Origin", "moz-extension://18443650-c9cb-407e-83ea-b7e0f5465571")
-	req.Header.Add("DNT", "1")
 	req.Header.Add("Connection", "keep-alive")
-	req.Header.Add("Sec-Fetch-Dest", "empty")
-	req.Header.Add("Sec-Fetch-Mode", "cors")
-	req.Header.Add("Sec-Fetch-Site", "cross-site")
-	req.Header.Add("TE", "trailers")
 
 	res, err := client.Do(req)
 	if err != nil {
@@ -54,16 +49,68 @@ func SetAvailability(id string, available bool, date string) {
 		return
 	}
 
+	var responseBody map[string]interface{}
+	err = json.Unmarshal(bodyBytes.Bytes(), &responseBody)
+	if err != nil {
+		fmt.Println("Erreur lors du décodage JSONe:", err)
+		return
+	}
+
+	if responseBody["msg"] == "OK" {
+		fmt.Println("Availability set")
+	} else {
+		fmt.Println("Error while setting availability")
+	}
+
+	UpdateAvailability(id)
+}
+
+func UpdateAvailability(id string) {
+	url := "https://api-p.pylote.io/logging/update"
+	method := "POST"
+
+	payload := strings.NewReader(fmt.Sprintf("{\"id\": \"%s\", \"variable\": \"m\", \"value\": \"%s\"}", id, time.Now().Format(time.RFC3339Nano)))
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	req.Header.Add("User-Agent", "Sylote")
+	req.Header.Add("Accept", "application/json, text/plain, */*")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Accept-Language", "en-US,en;q=0.5")
+	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+	req.Header.Add("Connection", "keep-alive")
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	var bodyBytes bytes.Buffer
+	_, err = io.Copy(&bodyBytes, res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(bodyBytes.String())
 	var body map[string]interface{}
 	err = json.Unmarshal(bodyBytes.Bytes(), &body)
 	if err != nil {
-		fmt.Println("Erreur lors du décodage JSON:", err)
+		fmt.Println("Erreur lors du décodage JSONfjfjfh:", err)
 		return
 	}
 
 	if body["msg"] == "OK" {
-		fmt.Println("Availability set")
+		fmt.Println("Logging updated")
 	} else {
-		fmt.Println("Error while setting availability")
+		fmt.Println("Error while updating availability")
 	}
 }
