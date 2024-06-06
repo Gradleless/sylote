d6eec159f569a5a096acb77f9e5767fde662fa6b
Lyciaa <57102194+Gradleless@users.noreply.github.com>
2024-06-06 18:52:12 +0200

    pylote change 4 variables, why ???

--- lib/pylote/availability.go
+++ lib/pylote/availability.go
@@ -69,48 +69,139 @@ func UpdateAvailability(id string) {
 	url := "https://api-p.pylote.io/logging/update"
 	method := "POST"
 
-	payload := strings.NewReader(fmt.Sprintf("{\"id\": \"%s\", \"variable\": \"m\", \"value\": \"%s\"}", id, time.Now().Format(time.RFC3339Nano)))
-
-	client := &http.Client{}
-	req, err := http.NewRequest(method, url, payload)
+	logs, err := GetLogs(id)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error fetching logs:", err)
 		return
 	}
 
-	req.Header.Add("User-Agent", "Sylote")
-	req.Header.Add("Accept", "application/json, text/plain, */*")
-	req.Header.Add("Content-Type", "application/json")
-	req.Header.Add("Accept-Language", "en-US,en;q=0.5")
-	req.Header.Add("Accept-Encoding", "gzip, deflate, br")
-	req.Header.Add("Connection", "keep-alive")
+	variablesToUpdate := map[string]interface{}{
+		"a":  logs.A + 1,
+		"m":  time.Now().Format(time.RFC3339Nano),
+		"cc": logs.Cc + 1,
+		"dd": logs.Dd + 1,
+	}
+
+	for key, value := range variablesToUpdate {
+		payload := strings.NewReader(fmt.Sprintf("{\"id\": \"%s\", \"variable\": \"%s\", \"value\": \"%v\"}", id, key, value))
+
+		client := &http.Client{}
+		req, err := http.NewRequest(method, url, payload)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		req.Header.Add("User-Agent", "Sylote")
+		req.Header.Add("Accept", "application/json, text/plain, */*")
+		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("Accept-Language", "en-US,en;q=0.5")
+		req.Header.Add("Accept-Encoding", "gzip, deflate, br")
+		req.Header.Add("Connection", "keep-alive")
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		defer res.Body.Close()
+
+		var bodyBytes bytes.Buffer
+		_, err = io.Copy(&bodyBytes, res.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var body map[string]interface{}
+		err = json.Unmarshal(bodyBytes.Bytes(), &body)
+		if err != nil {
+			fmt.Println("Erreur lors du décodage JSON (UpdateAvailability) :", err)
+			return
+		}
+
+		if body["msg"] == "OK" {
+			fmt.Printf("Logging updated for variable %s\n", key)
+		} else {
+			fmt.Printf("Error while updating variable %s\n", key)
+		}
+	}
+}
+
+func GetLogs(id string) (LogData, error) {
+	// ***REMOVED***4
+	url := "https://api-p.pylote.io/logging/getLogs/" + id
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		fmt.Println(err)
+		return LogData{}, err
+	}
 
 	res, err := client.Do(req)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return LogData{}, err
 	}
-
 	defer res.Body.Close()
 
 	var bodyBytes bytes.Buffer
 	_, err = io.Copy(&bodyBytes, res.Body)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return LogData{}, err
 	}
 
-	fmt.Println(bodyBytes.String())
-	var body map[string]interface{}
-	err = json.Unmarshal(bodyBytes.Bytes(), &body)
+	var logData LogData
+	err = json.Unmarshal(bodyBytes.Bytes(), &logData)
 	if err != nil {
-		fmt.Println("Erreur lors du décodage JSON (UpdateAvailability) :", err)
-		return
+		fmt.Println("Error decoding JSON (GetLogs):", err)
+		return LogData{}, err
 	}
 
-	if body["msg"] == "OK" {
-		fmt.Println("Logging updated")
-	} else {
-		fmt.Println("Error while updating availability")
-	}
+	fmt.Println("Log Data:", logData)
+	return logData, nil
+}
+
+// Made it with extra cause it'll be used in the future, maybe
+type LogData struct {
+	A                  int           `json:"a"`
+	B                  int           `json:"b"`
+	C                  int           `json:"c"`
+	M                  string        `json:"m"`
+	D                  int           `json:"d"`
+	E                  int           `json:"e"`
+	F                  int           `json:"f"`
+	G                  int           `json:"g"`
+	H                  int           `json:"h"`
+	N                  string        `json:"n"`
+	I                  int           `json:"i"`
+	J                  int           `json:"j"`
+	K                  string        `json:"k"`
+	T                  string        `json:"t"`
+	L                  int           `json:"l"`
+	Z                  string        `json:"z"`
+	P                  int           `json:"p"`
+	U                  string        `json:"u"`
+	Cc                 int           `json:"cc"`
+	Dd                 int           `json:"dd"`
+	Gg                 bool          `json:"gg"`
+	Id                 string        `json:"id"`
+	Hh                 []string      `json:"hh"`
+	ExcludeFromUsers   []interface{} `json:"Exclude (from Users)"`
+	W                  string        `json:"w"`
+	Y                  string        `json:"y"`
+	OpenToCDIFromUsers []interface{} `json:"Open_to_CDI (from Users)"`
+	PrenomNomFromUsers []string      `json:"Prénom Nom (from Users)"`
+	PhoneFromUsers     []string      `json:"phone (from Users)"`
+	MailPylote         []string      `json:"Mail Pylote"`
+	X                  string        `json:"x"`
+	DiffOuvDispo       int           `json:"Diff ouv dispo"`
+	Oo                 int           `json:"oo"`
+	Pp                 int           `json:"pp"`
+	Qq                 bool          `json:"qq"`
+	O                  []interface{} `json:"o"`
 }
